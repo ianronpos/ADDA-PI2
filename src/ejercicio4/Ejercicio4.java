34c08db06d13cef6d6a0ea7ea87870f9eaba3172
ian ronco postigo <ian.ronco2006@gmail.com>
2025-12-03 17:01:29 +0100

    Completo

--- src/ejercicio4/Ejercicio4.java
+++ src/ejercicio4/Ejercicio4.java
@@ -1,22 +1,25 @@
 package ejercicio4;
 
 
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Set;
 import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 import org.jgrapht.Graph;
 import org.jgrapht.GraphPath;
-import org.jgrapht.alg.interfaces.SpanningTreeAlgorithm;
-import org.jgrapht.alg.interfaces.SpanningTreeAlgorithm.SpanningTree;
+import org.jgrapht.alg.connectivity.ConnectivityInspector;
 import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
-import org.jgrapht.alg.spanning.KruskalMinimumSpanningTree;
 import org.jgrapht.alg.tour.HeldKarpTSP;
 import org.jgrapht.graph.SimpleGraph;
-import org.jgrapht.graph.SimpleWeightedGraph;
 
 import us.lsi.colors.GraphColors;
 import us.lsi.colors.GraphColors.Color;
-import us.lsi.graphs.Graphs2;
+import us.lsi.graphs.views.SubGraphView;
 
 
 public class Ejercicio4 {
@@ -29,7 +32,7 @@ public class Ejercicio4 {
 		Interseccion target = g.vertexSet().stream()
 				.filter(v-> v.getNombre().equals(mOut)).findFirst().get(); 
 		
-		
+		//Aplicas Dijkstra entro los dos vertices
 		GraphPath<Interseccion, Calle> res =  DijkstraShortestPath.findPathBetween(g, source, target); 
 		
 		GraphColors.toDot(g, ftest, 
@@ -44,8 +47,10 @@ public class Ejercicio4 {
 	
 	public static GraphPath<Interseccion,Calle> getRecorrido_E4B(Graph <Interseccion, Calle> g) {
         /*Traveling Salesperson Problem o de Ciclos Hamiltonianos.
-			
-		*/
+         * Consiste en buscar el camino mas corto en funcion del peso de las aristas 
+         * sin pasar 2 veces por ningun vertice y volver al vertice de partida
+         * */
+		//Aplicas TSP y obtienes el camino
 		HeldKarpTSP<Interseccion, Calle> TSP = new HeldKarpTSP<Interseccion, Calle>();
 		GraphPath<Interseccion, Calle> res = TSP.getTour(g); 
 		
@@ -60,7 +65,41 @@ public class Ejercicio4 {
 	}
 	
 	public static Graph<Interseccion,Calle> getRecorridoMaxRelevante_E4C(Set<Calle> cs,Graph <Interseccion, Calle> g, String ftest) {
+		//TODO: Preguntar si un vertice no tiene relevancia se suma 0 o -1
 		
+		//Predicados y funcion auxiliar
+		Predicate<Calle> callesNoCortadas = c-> !(cs.stream().mapToInt(Calle::getId).anyMatch(cId-> cId == c.getId())); 
+		Function<Set<Interseccion>, Integer> calculoRelevancia = s-> s.stream().mapToInt(Interseccion::getRelevancia).sum(); 
+		
+		//Crea un nuevo grafo que contenga todos los vertices y las calles que no estan cortadas 
+		Graph<Interseccion, Calle> gc = SubGraphView.ofEdges(g, callesNoCortadas);
+
+		//Recorremmos los vertices del grafo
+		ConnectivityInspector<Interseccion, Calle> cI = new ConnectivityInspector<Interseccion, Calle>(gc); 
+			
+		//Agrupa los camino con su relevancia
+		Map<Integer, List<Interseccion>> relevanciaCamino = cI.connectedSets().stream()
+				.collect(Collectors.toMap(
+						calculoRelevancia,
+						s-> s.stream().toList()
+						)); 
+		
+		//Escoje la lista con mas relevancia
+		//Usamos el get(relevanciaCamino.size()-1) por que no deja añadir un .reversed en el sorted
+		List<Interseccion> caminoElegido = relevanciaCamino.entrySet().stream()
+				.sorted(Map.Entry.comparingByKey())
+				.toList().get(relevanciaCamino.size()-1).getValue(); 
+		
+		//se pintan los vertices y aristas
+		GraphColors.toDot(gc, ftest, 
+				v->"INT-" + v.getId() + " Relevancia " + v.getRelevancia(), 
+				e->"", 
+				v->GraphColors.colorIf(Color.red, Color.black, caminoElegido.contains(v)), 
+				e->GraphColors.colorIf(Color.red, Color.black, 
+						caminoElegido.contains(gc.getEdgeSource(e)) || caminoElegido.contains(gc.getEdgeTarget(e)))
+				);
+		
+		return gc; 
 	}
 		
 }
